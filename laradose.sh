497484391a2423cf80266da5a2011a0878722995
AdrienPoupa <adrien@poupa.fr>
2020-06-11 00:27:46 -0400

    Fix indent

--- laradose.sh
+++ laradose.sh
@@ -6,10 +6,10 @@ VERSION=1.0.0
 
 install() {
   if [ -d ./docker ]; then
-    fatal "Laradose is already installed"
+  fatal "Laradose is already installed"
   fi
 
-	echo "Installing Laradose to the current directory..."
+  echo "Installing Laradose to the current directory..."
 
   copy_files
 
@@ -17,9 +17,9 @@ install() {
 
   configure
 
-	echo "Laradose was installed successfully!"
+  echo "Laradose was installed successfully!"
 
-	exit 0
+  exit 0
 }
 
 copy_files() {
@@ -27,21 +27,21 @@ copy_files() {
   verify_command unzip || fatal 'unzip is required for the installation script'
 
   echo "Downloading..."
-	download laradose.zip ${GITHUB_URL}/archive/master.zip
+  download laradose.zip ${GITHUB_URL}/archive/master.zip
 
   echo "Unzipping..."
-	unzip -q laradose.zip -d laradose
+  unzip -q laradose.zip -d laradose
 
   echo "Copying files..."
-	cp -r ./laradose/laradose-master/docker ./docker
-	cp ./laradose/laradose-master/docker-compose.yml ./
-	cp ./laradose/laradose-master/laradose.sh ./laradose.sh
+  cp -r ./laradose/laradose-master/docker ./docker
+  cp ./laradose/laradose-master/docker-compose.yml ./
+  cp ./laradose/laradose-master/laradose.sh ./laradose.sh
 
-	cat ./laradose/laradose-master/.env >> ./.env
-	cat ./laradose/laradose-master/.env >> ./.env.example
+  cat ./laradose/laradose-master/.env >> ./.env
+  cat ./laradose/laradose-master/.env >> ./.env.example
 
-	rm -rf ./laradose
-	rm ./laradose.zip
+  rm -rf ./laradose
+  rm ./laradose.zip
 }
 
 generate_ssl_certificate() {
@@ -55,21 +55,21 @@ generate_ssl_certificate() {
 }
 
 update() {
-	echo "Be aware that this command will overwrite any modifications you made to Laradose configuration or Docker images."
-	echo "Please make sure that your folder is versioned so you can revert to the previous state if needed."
-	read -r -p "Press any key to continue..."
-	echo "Updating Laradose..."
-	copy_files
-	exit 0
+  echo "Be aware that this command will overwrite any modifications you made to Laradose configuration or Docker images."
+  echo "Please make sure that your folder is versioned so you can revert to the previous state if needed."
+  read -r -p "Press any key to continue..."
+  echo "Updating Laradose..."
+  copy_files
+  exit 0
 }
 
 # Menu inspired by https://serverfault.com/a/298312
 additional_containers_menu() {
-    echo "Select the additional containers you want to enable:"
-    for i in "${!options[@]}"; do
-        printf "%d%s. %s\n" $((i+1)) "${choices[i]:-}" "${options[i]}"
-    done
-    if [[ "$msg" ]]; then echo "$msg"; fi
+  echo "Select the additional containers you want to enable:"
+  for i in "${!options[@]}"; do
+    printf "%d%s. %s\n" $((i+1)) "${choices[i]:-}" "${options[i]}"
+  done
+  if [[ "$msg" ]]; then echo "$msg"; fi
 }
 
 configure() {
@@ -80,16 +80,16 @@ configure() {
 
   prompt="Type the container number (again to uncheck, ENTER when done): "
   while additional_containers_menu && read -rp "$prompt" num && [[ "$num" ]]; do
-      [[ "$num" != *[![:digit:]]* ]] &&
-      (( num > 0 && num <= ${#options[@]} )) ||
-      { msg="Invalid option: $num"; continue; }
-      ((num--)); msg="${options[num]} was ${choices[num]:+un}enabled"
-      [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
+    [[ "$num" != *[![:digit:]]* ]] &&
+    (( num > 0 && num <= ${#options[@]} )) ||
+    { msg="Invalid option: $num"; continue; }
+    ((num--)); msg="${options[num]} was ${choices[num]:+un}enabled"
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
   done
 
   compose_file_input="docker-compose.yml:"
   for i in "${!options[@]}"; do
-      [[ "${choices[i]}" ]] && compose_file_input=$compose_file_input"/docker/${folders[i]}/docker-compose.override.yml":
+    [[ "${choices[i]}" ]] && compose_file_input=$compose_file_input"/docker/${folders[i]}/docker-compose.override.yml":
   done
 
   # Remove last :
@@ -107,11 +107,11 @@ configure() {
   env_input "DB_PORT" "MySQL port"
 
   if [[ $compose_file_input == *"redis"* ]]; then
-    env_input "REDIS_PORT" "Redis port"
+  env_input "REDIS_PORT" "Redis port"
   fi
 
   if [[ $compose_file_input == *"phpmyadmin"* ]]; then
-      env_input "PHPMYADMIN_PORT" "phpMyAdmin port"
+    env_input "PHPMYADMIN_PORT" "phpMyAdmin port"
   fi
 
   env_input "PHP_VERSION" "PHP Version (7.2, 7.3, 7.4)"
@@ -130,96 +130,96 @@ configure() {
 }
 
 env_input() {
-    key=$1
-    description=$2
+  key=$1
+  description=$2
 
-    read -r -p "$description: [${!key}] " new_value
+  read -r -p "$description: [${!key}] " new_value
 
-    if [ -n "$new_value" ]; then
-        sed -i "s/${key}=.*/${key}=${new_value}/" ./.env
-    fi
+  if [ -n "$new_value" ]; then
+    sed -i "s/${key}=.*/${key}=${new_value}/" ./.env
+  fi
 }
 
 uninstall() {
-	echo "Uninstalling Laradose..."
+  echo "Uninstalling Laradose..."
 
-	rm -rf ./docker
-	rm ./docker-compose.yml
-	rm ./.env.laradose
+  rm -rf ./docker
+  rm ./docker-compose.yml
+  rm ./.env.laradose
 
-	echo "Laradose was uninstalled successfully!"
-	echo "You can now remove additional entries from your .env and .env.example files"
+  echo "Laradose was uninstalled successfully!"
+  echo "You can now remove additional entries from your .env and .env.example files"
 
-	exit 0
+  exit 0
 }
 
 show_menus() {
-	clear
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "             L A R A D O S E"
-	echo
-	echo "Author: Adrien Poupa"
-	echo "Version: "${VERSION}
-	echo "URL: "${GITHUB_URL}
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "1. Install"
-	echo "2. Update"
-	echo "3. Configure"
-	echo "4. Uninstall"
-	echo "0. Exit"
+  clear
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "       L A R A D O S E"
+  echo
+  echo "Author: Adrien Poupa"
+  echo "Version: "${VERSION}
+  echo "URL: "${GITHUB_URL}
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "1. Install"
+  echo "2. Update"
+  echo "3. Configure"
+  echo "4. Uninstall"
+  echo "0. Exit"
 }
 
 read_options() {
-	local choice
-	read -r -p "Enter choice [1 - 4] " choice
-	case $choice in
-		1) install ;;
-		2) update ;;
-		3) configure ;;
-		4) uninstall ;;
-		*) exit 0;;
-	esac
+  local choice
+  read -r -p "Enter choice [1 - 4] " choice
+  case $choice in
+    1) install ;;
+    2) update ;;
+    3) configure ;;
+    4) uninstall ;;
+    *) exit 0;;
+  esac
 }
 
 # Credits https://raw.githubusercontent.com/rancher/k3s/master/install.sh
 verify_download() {
-    verify_command
+  verify_command
 
-    # Set verified executable as our downloader program and return success
-    DOWNLOADER=$1
-    return 0
+  # Set verified executable as our downloader program and return success
+  DOWNLOADER=$1
+  return 0
 }
 
 # --- verify existence of a command executable ---
 verify_command() {
-    # Return failure if it doesn't exist or is no executable
-    [ -x "$(command -v "$1")" ] || return 1
+  # Return failure if it doesn't exist or is no executable
+  [ -x "$(command -v "$1")" ] || return 1
 
-    return 0
+  return 0
 }
 
 download() {
-    [ $# -eq 2 ] || fatal 'download needs exactly 2 arguments'
+  [ $# -eq 2 ] || fatal 'download needs exactly 2 arguments'
 
-    case $DOWNLOADER in
-        curl)
-            curl -o "$1" -sfL "$2"
-            ;;
-        wget)
-            wget -qO "$1" "$2"
-            ;;
-        *)
-            fatal "Incorrect executable '$DOWNLOADER'"
-            ;;
-    esac
+  case $DOWNLOADER in
+    curl)
+      curl -o "$1" -sfL "$2"
+      ;;
+    wget)
+      wget -qO "$1" "$2"
+      ;;
+    *)
+      fatal "Incorrect executable '$DOWNLOADER'"
+      ;;
+  esac
 
-    # Abort if download command failed
-    [ $? -eq 0 ] || fatal 'Download failed'
+  # Abort if download command failed
+  [ $? -eq 0 ] || fatal 'Download failed'
 }
 
 fatal() {
-    echo '[ERROR]' "$@" >&2
-    exit 1
+  echo '[ERROR]' "$@" >&2
+  exit 1
 }
 
 while true
@@ -227,8 +227,8 @@ do
   verify_command docker || fatal 'Docker is required for Laradose'
   verify_command docker-compose || fatal 'docker-compose is required for Laradose'
   if ! [ -f ./.env ]; then
-    fatal 'You must have a .env file'
+  fatal 'You must have a .env file'
   fi
-	show_menus
-	read_options
+  show_menus
+  read_options
 done
